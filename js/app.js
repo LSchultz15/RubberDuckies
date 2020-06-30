3ba0e85255a9fa002bb9c3b36e73c7e299f7bdf5
Logan Schultz <loganadamschultz@gmail.com>
2020-06-30 14:10:46 -0500

    removed css folder and edited href to try and get css file to connect when in GitHub.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 'use strict';
-
-
-
 console.log('This file is connected and ready to run.');
 
+//if else and else if logic will go here
+
+var userName = prompt('Please enter your first name');
+
+alert('Hello, ' + userName + 'click ok to proceed');
